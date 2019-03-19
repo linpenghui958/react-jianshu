eacfa2f60a927af914a8a189a8cd6e022455f82c
linpenghui958@qq.com <linpenghui958@qq.com>
2019-03-19 23:08:18 +0800

    add home list

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 import { ListItem, ListInfo, LoadMore } from '../style';
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class List extends Component{
   render() {
+    const { list } = this.props
     return (
-      <div>List</div>
+      <div>
+        {
+					list.map((item, index) => {
+						return (
+							<Link key={index} to={'/detail/' + item.get('id')}>
+								<ListItem >
+									<img alt='' className='pic' src={item.get('imgUrl')} />
+									<ListInfo>
+										<h3 className='title'>{item.get('title')}</h3>
+										<p className='desc'>{item.get('desc')}</p>
+									</ListInfo>
+								</ListItem>
+							</Link>
+						);
+					})
+				}
+      </div>
     )
   }
 }
 
-export default List
+const mapStateToProps = state => {
+  return {
+    list: state.getIn(['home', 'articleList'])
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(List)
