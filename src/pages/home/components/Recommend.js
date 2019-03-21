4757a82b8bb86a814a98a1023f6ab3d0ae96f2ff
linpenghui958@qq.com <linpenghui958@qq.com>
2019-03-21 22:51:18 +0800

    add home basic frame

--- src/pages/home/components/Recommend.js
+++ src/pages/home/components/Recommend.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import { RecommendWrapper, RecommendItem } from '../style';
+import { connect } from 'react-redux'
 
 class Recommend extends Component {
 
   render() {
     return (
-      <div>Recommend</div>
+      <RecommendWrapper>
+        {
+          this.props.list.map((item) => {
+            return <RecommendItem imgUrl={item.get('imgUrl')} key={item.get('id')}/>
+          })
+        }
+      </RecommendWrapper>
     )
   }
 }
 
-export default Recommend
+const mapStateToProps = state => {
+  return {
+    list: state.getIn(['home', 'recommendList'])
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Recommend)
